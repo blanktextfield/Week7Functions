e28b7b175ff494808aff509c73a2219d5cfbbfcf
Opilane TTHK <opilane@tthk.ee>
2020-10-12 14:54:29 +0300

    Week7Functions

---
+++ Week7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Week7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What`s your gender (m/f)?");
+            string userInput = Console.ReadLine();
+            Hello(userInput);
+
+        }
+        public static void Hello(string gender)
+        {
+            if (gender == "f")
+            {
+                Console.WriteLine("Hello,madam!");
+            }
+            else if (gender == "m")
+            {
+                Console.WriteLine("Hello , sir!");
+            }
+            else
+            {
+                Console.WriteLine("Hello person");
+            }
+        }
+}   }
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+
+
+using System;
+
+namespace Week7Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your year of birth: ");
+            string userInput = Console.ReadLine();
+        }
+        public static void CalculateAge(string yearOfBirth)
+        {
+        int usersYearofBirth = Int32.Parse(yearOfBirth);
+        Console.WriteLine($"You are {2020 - usersYearofBirth} years old");
+        }
+    }
+}
